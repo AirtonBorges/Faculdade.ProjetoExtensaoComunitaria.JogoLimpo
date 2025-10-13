3e75128407dfb4342f84d65c6ad9fa381ffa1569
AirtonBorges <airtonborges2211@gmail.com>
2025-10-12 21:29:33 -0300

    fix: tentativa ajuste 4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
-// import { routes } from "@angular/service-worker";
 
 import { routes } from "./app.routes";
 
@@ -9,9 +8,5 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        // provideServiceWorker("ngsw-worker.js", {
-        //     enabled: false,
-        //     registrationStrategy: "registerWhenStable:30000",
-        // }),
     ],
 };
